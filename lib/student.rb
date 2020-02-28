8ae0964805d5e12f22e388d8572789b16c3e2802
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 22:30:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,40 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :id, :name, :grade
+  
+  def initialize(@id = NIL)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
+  def create_table
+    
+  end
+  
+  def drop_table
+    
+  end
+  
+  def save
+    
+  end
+  
+  def create
 
-
+  end
+  
+  def new_from_db
+    
+  end
+  
+  def find_by_name
+    
+  end
+  
+  def update
+    
+  end
+  
 end
