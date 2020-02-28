ab3361ba7833ffef91a3a450576ae5cff27498d3
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 23:20:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,11 @@ class Student
   end
   
   def self.find_by_name(name)
-	  sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first    
+	
+    sql = "SELECT * FROM students WHERE name = ?"
+
+    result = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(result)   
   end
   
   def update
