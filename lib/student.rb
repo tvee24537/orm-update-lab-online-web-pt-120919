ac8943ed677de48d568e31f8d984b2562fb9fcac
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 23:20:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,10 +49,8 @@ class Student
   end
   
   def self.new_from_db(id, name, grade)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)    
+    student_new = self.new(row[0], row[1], row[2])
+    student_new
   end
   
   def self.find_by_name(name)
