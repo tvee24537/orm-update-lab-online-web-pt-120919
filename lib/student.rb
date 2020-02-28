24e47a5780571c6c4f02a38ec7b67ba54000beb7
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-28 23:14:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   #  with DB[:conn]
   attr_accessor :id, :name, :grade
   
-  def initialize(@id = nil, name, grade)
+  def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
